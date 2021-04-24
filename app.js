bdbdbfb7d6c10c27d0f9bb9e8eb153160b0e8565
Mandy8055 <msankrit8055@gmail.com>
2021-04-25 03:29:25 +0530

    Added concurrency settings for the application

--- app.js
+++ app.js
@@ -1,27 +1,38 @@
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
+const throng = require("throng");
 
-const publicDirectory = path.join(__dirname, "./public");
+const WORKERS = process.env.WEB_CONCURRENCY || 1;
+throng(
+  {
+    workers: WORKERS,
+    lifetime: Infinity,
+  },
+  start
+);
+function start() {
+  const publicDirectory = path.join(__dirname, "./public");
 
-const app = express();
-const PORT = process.env.PORT;
+  const app = express();
+  const PORT = process.env.PORT;
 
-app.use(express.static(publicDirectory));
+  app.use(express.static(publicDirectory));
 
-// For parsing url-encoded bodies as sent by HTML forms
-app.use(express.urlencoded({ extended: false }));
-// Parse JSON bodies(as sent by the API)
-app.use(express.json());
+  // For parsing url-encoded bodies as sent by HTML forms
+  app.use(express.urlencoded({ extended: false }));
+  // Parse JSON bodies(as sent by the API)
+  app.use(express.json());
 
-app.use(cookieParser());
+  app.use(cookieParser());
 
-app.set("view engine", "hbs");
+  app.set("view engine", "hbs");
 
-// Define routes
-app.use("/", require("./routes/pages"));
-app.use("/auth", require("./routes/auth"));
+  // Define routes
+  app.use("/", require("./routes/pages"));
+  app.use("/auth", require("./routes/auth"));
 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+  });
+}
