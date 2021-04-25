2b20be08bd8a957a549ec1b39484c55a2449b123
Mandy8055 <msankrit8055@gmail.com>
2021-04-25 13:10:29 +0530

    Improved deployment efficiency

--- controllers/dbController.js
+++ controllers/dbController.js
@@ -6,10 +6,12 @@ dotenv.config({
 });
 
 const db = mysql.createPool({
+  connectionLimit: process.env.CONNECTION_LIMIT,
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE,
+  port: process.env.DB_PORT,
 }); // Recreate the connection, since
 // the old one cannot be reused.
 
